950e44ba10d49b609e68a6bf14d24b6c83d324e8
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-04-01 12:54:30 +1100

    Import FM2015 example.

---
+++ apps/swapcounter/client.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <Client.h>
+
+int run(void) {
+    int x = 3;
+    int y = 5;
+    unsigned int i;
+    i = cs_swap(&x, &y);
+    return 0;
+}
---
+++ apps/swapcounter/service.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <Service.h>
+
+static unsigned int counter;
+
+unsigned int
+ss_swap(int *a, int *b){
+    int temp = *a;
+    *a = *b; *b = temp;
+    counter++;
+    return counter;
+}
