8c897e8d130709a81feeee763fa9ae3b7ecfc108
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-05-23 19:00:00 +1000

    Update function signature in global include example.

--- apps/global-imports/global/components/Printer/src/main.c
+++ apps/global-imports/global/components/Printer/src/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <vec.h>
 
-void printer_print_string(char *str) {
+void printer_print_string(const char *str) {
     printf("%s", str);
 }
 void printer_print_vec(vec_t v) {
