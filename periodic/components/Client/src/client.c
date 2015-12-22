0e313cfe24a695e3a55b2d957f369909bdeeaec0
Ihor Kuz <ihor.kuz@nicta.com.au>
2015-12-22 18:18:19 +1100

    Added a new periodic scheduling test app.
    JIRA CAMKES-429

---
+++ periodic/components/Client/src/client.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <camkes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IPRINT(format, ...) printf("%s: " format, get_instance_name(), ##__VA_ARGS__); 
+
+#define MAX  100000llu
+#define RATE  10000llu
+
+uint64_t acc = 0;
+
+int run() {
+
+    IPRINT("--- Starting ---\n");
+
+    while (acc < MAX) {
+	acc *= 17;
+	acc++;
+	acc /= 17;
+	acc++;
+	if (acc % RATE == 0) {
+		IPRINT("acc = %llu\n", acc);
+	}
+    }
+
+    IPRINT("*** DONE %llu ***\n", acc);
+
+}
