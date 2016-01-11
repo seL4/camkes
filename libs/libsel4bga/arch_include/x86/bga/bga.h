70bf500f431920d788c99dc5043e5d6a16b5d9e2
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2016-01-11 21:09:18 +1100

    Add libsel4bga that is another dependency of aeroplage.

---
+++ libs/libsel4bga/arch_include/x86/bga/bga.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#ifndef _LIBGA_BGA_H_
+#define _LIBGA_BGA_H_
+
+#include <stdint.h>
+
+/* More information about the BGA device itself is available from
+ * http://wiki.osdev.org/Bochs_VBE_Extensions.
+ */
+
+/* Opaque pointer as a handle to the device. */
+typedef struct bga *bga_p;
+
+/* Initialise the driver. Returns NULL on failure.
+ *  framebuffer - A pointer to the device frame mapped into your address space.
+ *  ioport_write - Callback for writing to an IO port.
+ *  ioport_read - Callback for reading from an IO port.
+ */
+bga_p bga_init(void *framebuffer,
+    void (*ioport_write)(uint16_t port, uint16_t value),
+    uint16_t (*ioport_read)(uint16_t port));
+
+/* Destroy and clean up resources associated with an initialised device.
+ * Returns 0 on success, non-zero on failure.
+ */
+int bga_destroy(bga_p device);
+
+/* Get the version number of this device. */
+uint16_t bga_version(bga_p device);
+
+/* Set the mode of the device. Returns 0 on success. On error, the device might
+ * be left in an invalid state.
+ *  width - The width of the display in pixels.
+ *  height - The height of the display in pixels.
+ *  bpp - The bits per pixel.
+ */
+int bga_set_mode(bga_p device, unsigned int width, unsigned int height,
+    unsigned int bpp);
+
+/* Set the value of the pixel at (x, y). How value is interpreted is dependent
+ * on the current bits per pixel configuration. To know how to pass this value
+ * correctly, consult http://wiki.osdev.org/Bochs_VBE_Extensions. Returns 0 on
+ * success.
+ *
+ * This function is mainly for drawing simple output to the screen. For
+ * anything performance-critical you will want to use the raw frame buffer
+ * access below.
+ *
+ * XXX: This function has not been tested with BPPs other than 24.
+ */
+int bga_set_pixel(bga_p device, unsigned int x, unsigned int y, char *value);
+
+/* Get a pointer to the frame buffer. You can output to the screen by directly
+ * writing into this buffer. To do this correctly you will have to consult the
+ * Bochs documentation for formatting details.
+ */
+void *bga_get_framebuffer(bga_p device);
+
+#endif /* !_LIBBGA_BGA_H_ */
---
+++ libs/libsel4bga/src/arch-x86/bga.c
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+/* The code in this file was partially extracted from David Pollack's summer
+ * project, ssh://hg//data/hg_root/summer_students/davidp/sel4vga.
+ */
+
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <bga/bga.h>
+
+struct bga {
+    void *framebuffer;
+
+    /* IO port functions. */
+    uint16_t (*read)(uint16_t port);
+    void (*write)(uint16_t port, uint16_t value);
+
+    /* Current configuration. */
+    unsigned int width;
+    unsigned int height;
+    unsigned int bpp;
+};
+
+/* The BGA device is controlled by operating on two IO ports, first the index
+ * port to indicate what register you are trying to read/write and then the
+ * data port to read or write the actual register.
+ */
+static const uint16_t INDEX = 0x1ce;
+static const uint16_t DATA  = 0x1cf;
+static void write_data(bga_p device, uint16_t index, uint16_t data) {
+    assert(device != NULL);
+    device->write(INDEX, index);
+    device->write(DATA, data);
+}
+static uint16_t read_data(bga_p device, uint16_t index) {
+    assert(device != NULL);
+    device->write(INDEX, index);
+    return device->read(DATA);
+}
+
+/* After writing INDEX_ENABLED to the index port, you can write either 0x0000
+ * or 0x0001 to the data port to disable or enable the device respectively.
+ */
+static const uint16_t INDEX_ENABLED = 0x0004;
+static void disable(bga_p device) {
+    const uint16_t data_disable = 0x0000;
+    write_data(device, INDEX_ENABLED, data_disable);
+}
+static void enable(bga_p device) {
+    /* Note that we unconditionally use a linear frame buffer and clear the
+    * screen. Not ideal, but the emphasis is on simplicity in this driver.
+    */
+    const uint16_t data_enable = 0x0001;
+    const uint16_t lfb = 0x0040;
+    write_data(device, INDEX_ENABLED, data_enable | lfb);
+}
+
+uint16_t bga_version(bga_p device) {
+    assert(device != NULL);
+    const uint16_t index_id = 0x0000; /* Index to read version information. */
+    const uint16_t mask = (uint16_t)~0xb0c0;
+    uint16_t result = read_data(device, index_id);
+    return result & mask;
+}
+
+bga_p bga_init(void *framebuffer,
+        void (*ioport_write)(uint16_t port, uint16_t value),
+        uint16_t (*ioport_read)(uint16_t port)) {
+    bga_p device = (bga_p)malloc(sizeof(struct bga));
+    if (device == NULL) {
+        return device;
+    }
+
+    memset(device, 0, sizeof(*device));
+    device->framebuffer = framebuffer;
+    device->write = ioport_write;
+    device->read = ioport_read;
+
+    return device;
+}
+
+int bga_destroy(bga_p device) {
+    free(device);
+    return 0;
+}
+
+int bga_set_mode(bga_p device, unsigned int width, unsigned int height, unsigned int bpp) {
+    /* We need to disable the device to change these parameters. */
+    disable(device);
+
+    /* Relevant indicies to write to. */
+    const uint16_t x_resolution = 0x0001;
+    const uint16_t y_resolution = 0x0002;
+    const uint16_t bits_per_pixel = 0x0003;
+
+    /* Setup the requested settings. */
+    write_data(device, x_resolution, width);
+    device->width = width;
+    write_data(device, y_resolution, height);
+    device->height = height;
+    write_data(device, bits_per_pixel, bpp);
+    device->bpp = bpp;
+
+    /* Finally re-enable the device to have the settings take effect. */
+    enable(device);
+
+    return 0;
+}
+
+int bga_set_pixel(bga_p device, unsigned int x, unsigned int y, char *value) {
+    char *target;
+    unsigned int coord_factor;
+    size_t len;
+
+    /* XXX: None of these other than 24-bit have been tested and they are most
+     * likely incorrect.
+     */
+
+    switch (device->bpp) {
+        case 8: {
+            coord_factor = 1;
+            len = 1;
+            break;
+        } case 15:
+          case 16: {
+            coord_factor = 2;
+            len = 2;
+            break;
+        } case 24: {
+            coord_factor = 3;
+            len = 3;
+            break;
+        } case 32: {
+            coord_factor = 4;
+            len = 3;
+            break;
+        } default: {
+            /* Unsupported BPP. */
+            return -1;
+        }
+    }
+
+    /* Determine where we need to write and copy the pixel data over. */
+    target = ((char*)device->framebuffer) + (y * device->width + x) * coord_factor;
+    (void)memcpy(target, value, len);
+
+    return 0;
+}
+
+void *bga_get_framebuffer(bga_p device) {
+    return device->framebuffer;
+}
