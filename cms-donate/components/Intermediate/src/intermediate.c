c8b5cca7ec94e00c96fd4f203ef51cd350ecffa1
Siwei Zhuang <siwei.zhuang@nicta.com.au>
2016-01-20 11:54:36 +1100

    Add cms-donate test.

---
+++ cms-donate/components/Intermediate/src/intermediate.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <camkes.h>
+#include <string.h>
+
+void r__init(void) {
+}
+
+char * r_echo_string(const char *s) {
+    return i_echo_string(s);
+}
+
+int r_echo_int(int i) {
+    return i_echo_int(i);
+}
+
+float r_echo_float(float f) {
+    return i_echo_float(f);
+}
+
+double r_echo_double(double d) {
+    return i_echo_double(d);
+}
+
+int r_echo_mix(double d) {
+    return i_echo_mix(d);
+}
+
+int r_echo_parameter(int pin, int *pout) {
+    return i_echo_parameter(pin, pout);
+}
+
