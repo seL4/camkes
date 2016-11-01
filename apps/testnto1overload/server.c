27a27e3ca634fb7f575796a85382fcbb888043e5
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2016-11-01 17:17:20 +1100

    Renamed _get_badge to _get_sender_id in test

--- apps/testnto1overload/server.c
+++ apps/testnto1overload/server.c
@@ -14,16 +14,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-extern seL4_Word f_get_badge(void);
-
 void f_foo(void) {
     static seL4_Word badge;
     static bool not_first;
     if (!not_first) {
-        badge = f_get_badge();
+        badge = f_get_sender_id();
         not_first = true;
     } else {
-        seL4_Word new_badge = f_get_badge();
+        seL4_Word new_badge = f_get_sender_id();
         assert(new_badge != badge);
         if (new_badge != badge) {
             printf("All OK\n");
