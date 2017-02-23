38d94810af8517c69b006de220beeacf13e2fd1e
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2017-02-23 15:22:29 +1100

    Added alignment test app

---
+++ apps/alignment/client.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+
+#include <camkes.h>
+#include <stdio.h>
+
+#include "common.h"
+
+int run() {
+    printf("Calling server...\n");
+    a_f();
+    printf("Back from server!\n");
+    printf("Client is testing alignment...\n");
+    test_alignment();
+    printf("All is well in the universe!\n");
+
+    return 0;
+}
---
+++ apps/alignment/common/common.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+
+/* A couple of operations that should trigger faults on x86 if
+ * our stack isn't set up correctly.
+ */
+
+#pragma once
+
+#include <string.h>
+
+typedef struct foo {
+    float arr[4];
+} foo_t;
+
+/* Hopefully the compiler will attempt to optimize this
+ * function with sse instructions on x86.
+ */
+static inline void memcpy_test(foo_t *a, foo_t *b) {
+    foo_t intermediate = *a;
+    intermediate.arr[1] += 0.5;
+    memcpy(b, &intermediate, sizeof(foo_t));
+}
+
+/* On x86, uses the sse instruction "movaps" to move the
+ * value pointed to by the stack pointer into the xmm0
+ * register. This instruction requires that address
+ * operands be 16 byte aligned.
+ */
+static inline void movaps_test(void) {
+#ifdef CONFIG_ARCH_X86
+    asm volatile("movaps (%%esp), %%xmm0\n":::"xmm0");
+#endif
+}
+
+/* Hopefully this function will result in a GP fault due
+ * to unaligned operands of sse instructions with alignment
+ * requirements, unless the stack is correctly aligned.
+ */
+static inline void test_alignment(void) {
+    foo_t a, b;
+    a.arr[0] = 0.1;
+    a.arr[1] = 0.2;
+    a.arr[2] = 0.3;
+    a.arr[3] = 0.4;
+
+    memcpy_test(&a, &b);
+    movaps_test();
+
+    printf("Done testing alignment: %f\n", b.arr[1]);
+}
