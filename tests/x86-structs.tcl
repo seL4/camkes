81e17891b32b96293bdc3ac3bc8cd5ba1895675c
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-03-30 15:52:12 +1100

    CAMKES-584: Add configs and tests for structs app

---
+++ tests/x86-structs.tcl
@@ -0,0 +1,23 @@
+#!/usr/bin/expect -f
+#
+# Copyright 2017, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+source [file join [file dirname [info script]] procs.tcl]
+
+set timeout 600
+
+spawn make x86_structs_defconfig
+check_exit
+
+source [file join [file dirname [info script]] build.tcl]
+
+source [file join [file dirname [info script]] run-x86.tcl]
+wait_for "array_in_struct: array length: 4, first element 3"
