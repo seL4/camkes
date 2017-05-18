ce79ccd0f6de9ee90ce055cbd14263938598e91d
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-05-18 16:02:23 +1000

    swapcounter: fix swapcounter test

--- apps/swapcounter/client.c
+++ apps/swapcounter/client.c
@@ -15,5 +15,8 @@ int run(void) {
     int y = 5;
     unsigned int i;
     i = cs_swap(&x, &y);
+    /* Printf for tests,
+     * you should probably remove this line if wanting to run proofs */
+    printf("done\n");
     return 0;
 }
--- tests/arm-swapcounter.tcl
+++ tests/arm-swapcounter.tcl
@@ -19,4 +19,4 @@ check_exit
 source [file join [file dirname [info script]] build.tcl]
 
 source [file join [file dirname [info script]] run-arm.tcl]
-wait_for "Done; suspending"
+wait_for "done"
