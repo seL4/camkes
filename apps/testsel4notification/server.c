d03717c5b2360316480e3dc462246cf352b4121f
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-07-05 15:02:42 +1000

    Rexpand licenses

--- apps/testsel4notification/server.c
+++ apps/testsel4notification/server.c
@@ -1,4 +1,14 @@
-/* @TAG(DATA61_BSD) */
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
 
 /* Server of seL4Notification testing application. The only thing we implement here
  * is a function to effectively expose `e_emit` to our client.
