295fc0578f65f926378bee0c06e153ab5ebee048
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-05-03 16:43:12 +1000

    trivial: Style rumprun_common.cmake

--- apps/rumprun_common.cmake
+++ apps/rumprun_common.cmake
@@ -21,23 +21,34 @@ include(ExternalProject)
 
 function(DeclareExternalRumprunProject external_target_name source_dir files)
 
-	add_custom_target(${external_target_name}-toolchains)
-	add_dependencies(${external_target_name}-toolchains rumprun_toplevel_support)
-	# Add ExternalProject for building a binary using rumprun toolchains
-	set(stamp_dir ${CMAKE_CURRENT_BINARY_DIR}/${external_target_name}-stamp)
-	ExternalProject_Add(${external_target_name}
-	    SOURCE_DIR ${source_dir}
-	    INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}
-	    STAMP_DIR ${stamp_dir}
-	    DEPENDS ${external_target_name}-toolchains
-	    BUILD_ALWAYS ON
-	    EXCLUDE_FROM_ALL
-	    CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=$<TARGET_PROPERTY:rumprun_toplevel_support,RUMPRUN_TOOLCHAIN_CMAKE>
-	               -G Ninja
-	               -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_CURRENT_BINARY_DIR}
-	)
-	DeclareExternalProjObjectFiles(${external_target_name} ${CMAKE_CURRENT_BINARY_DIR} FILES ${files})
-
-
+    add_custom_target(${external_target_name}-toolchains)
+    add_dependencies(${external_target_name}-toolchains rumprun_toplevel_support)
+    # Add ExternalProject for building a binary using rumprun toolchains
+    set(stamp_dir ${CMAKE_CURRENT_BINARY_DIR}/${external_target_name}-stamp)
+    ExternalProject_Add(
+        ${external_target_name}
+        SOURCE_DIR
+        ${source_dir}
+        INSTALL_DIR
+        ${CMAKE_CURRENT_BINARY_DIR}
+        STAMP_DIR
+        ${stamp_dir}
+        DEPENDS
+        ${external_target_name}-toolchains
+        BUILD_ALWAYS
+        ON
+        EXCLUDE_FROM_ALL
+        CMAKE_ARGS
+        -DCMAKE_TOOLCHAIN_FILE=$<TARGET_PROPERTY:rumprun_toplevel_support,RUMPRUN_TOOLCHAIN_CMAKE>
+        -G
+        Ninja
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_CURRENT_BINARY_DIR}
+    )
+    DeclareExternalProjObjectFiles(
+        ${external_target_name}
+        ${CMAKE_CURRENT_BINARY_DIR}
+        FILES
+        ${files}
+    )
 
 endfunction()
